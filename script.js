fd58120fd8328f1cf58a979beca016965d686748
vipulsharma1204 <43072665+vipulsharma1204@users.noreply.github.com>
2021-10-18 20:19:15 +0530

    first demo on js

---
+++ script.js
@@ -0,0 +1,31 @@
+var def = function(){    
+    setTimeout(() => {
+        console.log('Anubhav');
+    }, 3000);
+}
+var inc = function(){
+    var x = parseInt(document.getElementById("mynum").innerHTML);
+    x += 1;
+    if(x<10) document.getElementById("mynum").innerHTML = x;
+}
+
+var dec = function(){
+    var x = parseInt(document.getElementById("mynum").innerHTML);
+    x -= 1;
+    if(x>-1) document.getElementById("mynum").innerHTML = x;
+}
+var inc_app = function(){
+    var x = parseInt(document.getElementById("quan_app").innerHTML);
+    x += 1;
+    if(x<10) document.getElementById("quan_app").innerHTML = x;
+    document.getElementById("amount_app").innerHTML = x*parseInt(document.getElementById("price_app").innerHTML);
+}
+
+var dec_app = function(){
+    var x = parseInt(document.getElementById("quan_app").innerHTML);
+    x -= 1;
+    if(x>-1){
+        document.getElementById("quan_app").innerHTML = x;
+        document.getElementById("amount_app").innerHTML = x*parseInt(document.getElementById("price_app").innerHTML);
+    }
+}
